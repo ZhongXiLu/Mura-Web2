3e3d5fc00c8f44103865e2cf89a1097a4992426a
ZhongXiLu <zhongxilu@hotmail.com>
2020-10-01 15:06:28 +0200

    Added few more units tests

--- mura-web/src/app/services/analysis.service.spec.ts
+++ mura-web/src/app/services/analysis.service.spec.ts
@@ -1,17 +1,62 @@
 import { TestBed } from '@angular/core/testing';
-import { HttpClientModule } from '@angular/common/http';
+import { HttpClient, HttpErrorResponse } from '@angular/common/http';
+import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
 
 import { AnalysisService } from './analysis.service';
 
+const mockResponse = [
+  {
+    "id": 1,
+    "repoName": "slub/urnlib",
+    "gitRepo": "https://github.com/slub/urnlib",
+    "report": "report.html",
+    "mutants": "mutants.csv",
+    "startTime": "2020-10-01 09:08 GMT",
+    "finished": true,
+    "successful": true,
+    "errorMessage": ""
+  }
+]
+
 describe('AnalysisService', () => {
+  let httpClient: HttpClient;
+  let httpTestingController: HttpTestingController;
   let service: AnalysisService;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({imports: [HttpClientModule]});
+    TestBed.configureTestingModule({
+      imports: [ HttpClientTestingModule ],
+      providers: [ AnalysisService ]
+    });
+    httpClient = TestBed.get(HttpClient);
+    httpTestingController = TestBed.get(HttpTestingController);
     service = TestBed.inject(AnalysisService);
+  });
+
+  afterEach(() => {
+    httpTestingController.verify();
   });
 
   it('should be created', () => {
     expect(service).toBeTruthy();
   });
+
+  it('should return all analyses', () => {
+    service.getAllAnalyses().subscribe(
+      res => expect(res).toEqual(mockResponse, 'should return a list of all the analyses'),
+      fail
+    );
+
+    httpTestingController.expectOne(service.analysisUrl).flush(mockResponse);
+  });
+
+  it('should return empty list if failed contacting backend', () => {
+    service.getAllAnalyses().subscribe(
+      res => expect(res.length).toEqual(0, 'should return an empty list'),
+      fail
+    );
+
+    httpTestingController.expectOne(service.analysisUrl).flush("505 Error", {status: 500, statusText: 'Internal Server Error'});
+  });
+
 });
--- mura-web/src/app/services/analysis.service.ts
+++ mura-web/src/app/services/analysis.service.ts
@@ -1,21 +1,26 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Analysis } from '../models/analysis';
-import { Observable } from 'rxjs';
+import { Observable, of } from 'rxjs';
+import { catchError } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AnalysisService {
 
-    private analysisUrl: string;
+    analysisUrl: string;
 
     constructor(private http: HttpClient) {
         this.analysisUrl = "http://localhost:8080/analysis"
     }
 
     public getAllAnalyses(): Observable<Analysis[]> {
-        return this.http.get<Analysis[]>(this.analysisUrl);
+        return this.http.get<Analysis[]>(this.analysisUrl).pipe(
+          catchError(error => {
+            return of([]);
+          })
+        );
     }
 
 }
